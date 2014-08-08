a7d0411fbc9811a9d1f17622eb108511b8973853
qizh <qizh@microsoft.com>
2014-08-08 14:27:02 +0800

    Feature - add TagStatistics
    TagStatistics will parse the stack overflow data dump and generate fact table for tags and dates, as well as dimension table for tags and dates. In addition, it can generate graph for tag relations.
    
    + Add TagStatistics project.

---
+++ TagStatistics/GraphBuilder.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TagStatistics
+{
+    class Node
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "group")]
+        public int Group { get; set; }
+    }
+
+    class Link
+    {
+        [JsonProperty(PropertyName = "source")]
+        public int Source { get; set; }
+
+        [JsonProperty(PropertyName = "target")]
+        public int Target { get; set; }
+
+        [JsonProperty(PropertyName = "value")]
+        public int Value { get; set; }
+    }
+
+    class Graph
+    {
+        [JsonProperty(PropertyName = "nodes")]
+        public Node[] Nodes { get; set; }
+
+        [JsonProperty(PropertyName = "links")]
+        public List<Link> Links { get; set; }
+    }
+
+    class GraphBuilder
+    {
+        public Graph Build(Dictionary<string, int> tags, Dictionary<string, Dictionary<string, int>> relations)
+        {
+            int magic = 10000;
+
+            // Prunes the tree to remove edges with little weight.
+            foreach (var relation in relations)
+            {
+                foreach (var node in relation.Value.Keys.Where(k => relation.Value[k] < magic).ToList())
+                {
+                    relation.Value.Remove(node);
+                }
+            }
+
+            // Prunes the tree to remove nodes with no edges.
+            foreach (var node in relations.Keys.Where(k => relations[k].Count == 0).ToList())
+            {
+                relations.Remove(node);
+            }
+
+            // Builds the tags based on the pruned tree.
+            tags.Clear();
+            foreach (var relation in relations)
+            {
+                int val;
+                if (!tags.TryGetValue(relation.Key, out val))
+                    tags[relation.Key] = tags.Count;
+
+                foreach (var target in relation.Value)
+                {
+                    if (!tags.TryGetValue(target.Key, out val))
+                        tags[target.Key] = tags.Count;
+                }
+            }
+
+            // Builds the graph json object for d3.
+            var graph = new Graph();
+
+            graph.Nodes = new Node[tags.Count];
+            graph.Links = new List<Link>();
+
+            foreach (var tag in tags)
+            {
+                var node = new Node();
+
+                node.Name = tag.Key;
+                node.Group = 1;
+
+                graph.Nodes[tag.Value] = node;
+            }
+
+            foreach (var relation in relations)
+            {
+                foreach (var node in relation.Value)
+                {
+                    var link = new Link();
+
+                    link.Source = tags[relation.Key];
+                    link.Target = tags[node.Key];
+                    link.Value = (int)Math.Sqrt(node.Value / magic);
+
+                    graph.Links.Add(link);
+                }
+            }
+
+            return graph;
+        }
+    }
+}
---
+++ TagStatistics/TagStatistics.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace TagStatistics
+{
+    /// <summary>
+    /// Normalizes the stackoverflow data dump is too large, we'll need to normalize the entities such that they can get
+    /// imported into Power Pivot for visualization.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tags = new Dictionary<string, int>();
+            var days = new Dictionary<DateTime, int>();
+            var relations = new Dictionary<string, Dictionary<string, int>>();
+
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: TagStatistics StackOverflowXmlFile OutputDirectory");
+                return;
+            }
+
+            // Normalizes the stack overflow dump file. The mapping between posts and tags, tag dimension and date dimension will
+            // be stored in post file, tags dictionary and days dictionary respectively. Also, the relationships between tags
+            // will be stored in relations graph.
+            using (var writer = new StreamWriter(Path.Combine(args[1], "Posts.csv")))
+            {
+                using (var reader = XmlReader.Create(args[0]))
+                {
+                    int i = 0;
+                    var parser = new PostParser(reader, writer);
+
+                    while (reader.Read())
+                    {
+                        if (++i % 100 == 0)
+                            Console.WriteLine("{0} posts proceeded", i);
+
+                        parser.ProcessPost(tags, days, relations);
+
+                        // Uncommnet the following for testing on a small set of data.
+                        //if (i > 5000)
+                        //    break;
+                    }
+
+                    Console.WriteLine("{0:D} posts proceeded in total", i);
+                }
+            }
+
+            // Generates the tag dimension file.
+            using (var writer = new StreamWriter(Path.Combine(args[1], "Tags.csv")))
+            {
+                foreach (var key in tags.Keys)
+                {
+                    writer.WriteLine("{0},{1}", tags[key], key);
+                }
+            }
+
+            // Generates the tag dimension file.
+            using (var writer = new StreamWriter(Path.Combine(args[1], "Days.csv")))
+            {
+                foreach (var key in days.Keys)
+                {
+                    writer.WriteLine("{0},{1}", days[key], key);
+                }
+            }
+
+            // Generates the json object for d3 graph from relation graph.
+            using (var writer = new StreamWriter(Path.Combine(args[1], "Graph.js")))
+            {
+                var serializer = new JsonSerializer();
+                var builder = new GraphBuilder();
+
+                using (var jswriter = new JsonTextWriter(writer))
+                {
+                    jswriter.Formatting = Newtonsoft.Json.Formatting.Indented;
+
+                    serializer.Serialize(jswriter, builder.Build(tags, relations));
+                }
+            }
+        }
+    }
+}
