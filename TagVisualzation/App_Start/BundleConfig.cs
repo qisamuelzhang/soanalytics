e9f439f0d979545954d8aab5bc008214a55e597f
qizh <qizh@microsoft.com>
2014-08-08 15:12:52 +0800

    Feature - Add TagVisualization
    TagVisualization is a web app to visualize the tag relationship graph generated by TagStatistics project.
    
    + Add TagVisualzation.

---
+++ TagVisualzation/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TagVisualzation
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include("~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js", "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/d3").Include("~/Scripts/d3.js"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
